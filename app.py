e741a5539563f59d3084426aef61639ecb9acba1
Satyam <satyamsrivastava202@gmail.com>
2026-09-03 17:37:23 +0530

    Commit Some Changes

--- app.py
+++ app.py
@@ -8,3 +8,5 @@ link = st.text_input("Enter Youtube Link") #Getting the Link
 id = comments.get_video_id(link) #Getting the id
 
 top_10_comments = comments.get_comment_top_10(id) 
+
+st.write(top_10_comments)
--- comments.py
+++ comments.py
@@ -7,37 +7,42 @@ import googleapiclient.discovery
 load_dotenv()
 
 def get_video_id(link): 
-    pattern = r"^(https?://)?(www\.)?(youtube\.com/watch\?v=|youtu\.be/|youtube\.com/embed/)([a-zA-Z0-9_-]{11})$" 
-    link = link.strip()
-    match = re.fullmatch(pattern, link) #Using Regex for verification
+    match = None
+    video_id = None
+    if link:
+        pattern = r"^(https?://)?(www\.)?(youtube\.com/watch\?v=|youtu\.be/|youtube\.com/embed/)([a-zA-Z0-9_-]{11})$" 
+        link = link.strip()
+        match = re.fullmatch(pattern, link) #Using Regex for verification
     if match:
         video_id = match.group(4)
+
     return video_id     
 
 def get_comment_top_10(video_id): 
-    os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "0"
+    if video_id:
+        os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "0"
 
-    api_service_name = "youtube"
-    api_version = "v3"
-    DEVELOPER_KEY = os.getenv("YOUTUBE-API")
+        api_service_name = "youtube"
+        api_version = "v3"
+        DEVELOPER_KEY = os.getenv("YOUTUBE-API")
 
-    youtube = googleapiclient.discovery.build(
-        api_service_name, api_version, developerKey = DEVELOPER_KEY)
+        youtube = googleapiclient.discovery.build(
+            api_service_name, api_version, developerKey = DEVELOPER_KEY)
 
-    request = youtube.commentThreads().list(
-        videoId=video_id, order="relevance", maxResults=10,
-        part="snippet"
-    )
-    response = request.execute()
+        request = youtube.commentThreads().list(
+            videoId=video_id, order="relevance", maxResults=10,
+            part="snippet"
+        )
+        response = request.execute()
 
-    formatted_comments = [] #Getting the comment text with commenter name
+        formatted_comments = [] #Getting the comment text with commenter name
 
-    for item in response.get('items', [])[:5]:
-        snippet = item['snippet']['topLevelComment']['snippet']
-        name = snippet['authorDisplayName']
-        text = snippet['textOriginal']
-        
-        # Combine them into: Name: "Comment Text"
-        formatted_comments.append(f'{name}: "{text}"')
+        for item in response.get('items', [])[:10]:
+            snippet = item['snippet']['topLevelComment']['snippet']
+            name = snippet['authorDisplayName']
+            text = snippet['textOriginal']
+            
+            # Combine them into: Name: "Comment Text"
+            formatted_comments.append(f'{name}: "{text}"')
 
-    return formatted_comments
+        return formatted_comments

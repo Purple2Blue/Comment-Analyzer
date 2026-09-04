67cebca06b365c705a21f0097ad3beb60a49f8be
Satyam <satyamsrivastava202@gmail.com>
2026-09-04 21:21:52 +0530

    Used session_state to store video_id

--- app.py
+++ app.py
@@ -10,20 +10,24 @@ if "video_id" not in st.session_state:
 
 link = st.text_input("Enter Youtube Link") #Getting the Link
 
-
 if link:
-    id = comments.get_video_id(link) #Getting the id
+    st.session_state.video_id = comments.get_video_id(link) #Getting the id
 else:
-    st.warning("Please enter a valid Youtube link.")
+    st.info("Please enter a valid Youtube link.")
 
-top_10_comments = comments.get_comment_top_10(id) 
+if st.session_state.video_id:
+    top_10_comments = comments.get_comment_top_10(st.session_state.video_id)
 
-for item in top_10_comments:
-    match = re.match(r'@(\w+): "(.*)"$', item, re.DOTALL)
-    if match:
-        author = match.group(1)
-        comment = match.group(2)
-        
-        st.write(f"Author: {author}")
-        st.write(f"Comment: {comment}")
-        st.write("---")
+    if top_10_comments:
+        for item in top_10_comments:
+            match = re.match(r'@(\w+): "(.*)"$', item, re.DOTALL)
+            if match:
+                author = match.group(1)
+                comment = match.group(2)
+                
+                st.write(f"Author: {author}")
+                st.write(f"Comment: {comment}")
+                st.write("---")
+        if not match:
+            st.warning("Video doesn't have enough comments")
+
